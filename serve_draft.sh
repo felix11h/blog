a6ecf8908f8789d6ed7f2625f0586d62a0fdc03e
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-02-21 17:07:43 +0100

    use jekyll 3.7.2 docker image

--- serve_draft.sh
+++ serve_draft.sh
@@ -3,6 +3,6 @@
 docker run -it -p 127.0.0.1:4003:4000 \
        --user="$(id -u):$(id -g)" \
        -v $(pwd):/blog \
-       felix11h/jekyll3_env \
+       felix11h/jekyll3_env:j3.7.2 \
        /bin/bash -c \
        'jekyll serve --host=0.0.0.0 --draft'
